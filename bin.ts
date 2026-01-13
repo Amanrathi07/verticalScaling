b255f4a1bbc6ff745f31c587455e608164aca640
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-13 19:54:12 +0530

    add bin.ts for child worker

---
+++ bin.ts
@@ -0,0 +1,13 @@
+import cluster from "cluster" ;
+import os from "os" ;
+import { app } from ".";
+
+
+const coreCount = os.cpus().length ;
+
+
+if(cluster.isPrimary){
+    cluster.fork()
+}else{
+    app.listen(3000)
+}
--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
-import cluster from "cluster" ;
-import os from "os" ;
-
 import express from "express" ;
-
-const coreCount = os.cpus().length ;
 
 export const app = express();
 
